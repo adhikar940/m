b5611f147db858e5112f1f54b16c8e0eaebffbe8
adhikar940 <adhikar940@gmail.com>
2020-12-10 03:09:41 -0700

    mn

--- n/models.py
+++ n/models.py
@@ -178,7 +178,7 @@ class Rajyasabha(Parliament):
     total_member = models.IntegerField(null=True)
     MPname = models.CharField(max_length=300, default='')
 
-    elected = models.CharField(max_length=500, choices=choice, default='')
+    elect = models.CharField(max_length=500, choices=choice, default='')
 
 
     def __str__(self):
