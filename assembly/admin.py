b4a0ff6011f4d347f9da76de62967799bd745de0
mohan <adhikar940@gmail.com>
2022-01-24 23:06:44 +0530

    mn241

--- assembly/admin.py
+++ assembly/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
+from import_export.admin import ImportExportModelAdmin
+from . models import *
+admin.site.register(Assembly_Constituency1, ImportExportModelAdmin)
+admin.site.register(Legislative_Assembly1, ImportExportModelAdmin)
+admin.site.register(assemblypersonal1, ImportExportModelAdmin)
+admin.site.register(assemblyterm, ImportExportModelAdmin)
