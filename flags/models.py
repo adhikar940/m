57fa2ea9a5646171366ce1978acd4d7c6759d4bb
mohan <adhikar940@gmail.com>
2022-02-18 23:13:28 +0530

    mnd

--- flags/models.py
+++ flags/models.py
@@ -9,10 +9,11 @@ class flag(models.Model):
         ('p', 'p'),
     )
     name = (
-        ('red', 'red'),
-        ('white', 'white'),
-        ('blue', 'blue'),
+        ('red1', 'red1'),('red2', 'red2'),('red3', 'red3'),('red4', 'red4'),('red5', 'red5'),
+        ('white1', 'white1'),('white2', 'white2'),('white3', 'white3'),('white4', 'white4'),('white5', 'white5'),
+        ('blue1', 'blue1'),('blue2', 'blue2'),('blue3', 'blue3'),('blue4', 'blue4'),('blue5', 'blue5'),
     )
+
     Status = models.CharField(max_length=10, choices=status, default='v')
     Highlight = models.CharField(max_length=10, choices=highlight, default='n')
     color = models.CharField(max_length=10,choices=name, default='blue')
@@ -26,9 +27,9 @@ class flag(models.Model):
         return '%s' %(self.color)
 class flag1(models.Model):
     name = (
-        ('red1', 'red1'),('red2', 'red2'),('red3', 'red3'),('red4', 'red4'),('red5', 'red5'),
-        ('white1', 'white1'),('white2', 'white2'),('white3', 'white3'),('white4', 'white4'),('white5', 'white5'),
-        ('blue1', 'blue1'),('blue2', 'blue2'),('blue3', 'blue3'),('blue4', 'blue4'),('blue5', 'blue5'),
+        ('red', 'red'),
+        ('white', 'white'),
+        ('blue', 'blue'),
     )
     name = models.CharField(max_length=10,choices=name, default='red1' )
     activate =  models.CharField(max_length=10, choices=highlight, default='n')
