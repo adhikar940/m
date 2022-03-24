3e44980a2b4706c027093ea1400dda9309dec5e5
mohan <adhikar940@gmail.com>
2022-03-24 09:39:45 +0530

    k

--- assembly/admin.py
+++ assembly/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin,ImportExportActionModelAdmin
 from . models import *
 from import_export import resources
-admin.site.register(Assembly_Constituency1, ImportExportModelAdmin)
+'''
+class Assembly_Constituency1Resource(resources.ModelResource):
+    class Meta:
+        model = Assembly_Constituency1
+class Assembly_Constituency1Admin(ImportExportModelAdmin):
+    #list_display=('MLA_name',)
+    list_filter=('State','Assembly_Constituency_Name')
+    resource_class = Assembly_Constituency1Resource
+admin.site.register(Assembly_Constituency1, Assembly_Constituency1Admin)'''
+
 #admin.site.register(Legislative_Assembly1, ImportExportModelAdmin)
 class Legislative_Assembly1Resource(resources.ModelResource):
     class Meta:
@@ -11,9 +20,8 @@ class Legislative_Assembly1Admin(ImportExportModelAdmin):
     #list_display=('MLA_name',)
     list_filter=('state','Party')
     resource_class = Legislative_Assembly1Resource
-'''class Legislative_Assembly1Admin(ImportExportActionModelAdmin):
-    pass'''
 admin.site.register(Legislative_Assembly1, Legislative_Assembly1Admin)
+
 admin.site.register(assemblypersonal1, ImportExportModelAdmin)
 admin.site.register(assemblyterm, ImportExportModelAdmin)
 admin.site.register(excelupload, ImportExportModelAdmin)
