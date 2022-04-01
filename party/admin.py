71aac1b982d7f528b3d327daa5b89eb9c516f218
mohan <adhikar940@gmail.com>
2022-04-01 09:55:30 +0530

    party

--- party/admin.py
+++ party/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin,ImportExportActionModelAdmin
 from . models import *
+from import_export import resources
+class Party1Resource(resources.ModelResource):
+    class Meta:
+        model = Party1
+class Party1Admin(ImportExportModelAdmin):
+    #list_display=('MLA_name',)
+    search_fields = ['partyname','abbreviation']
+    resource_class = Party1Resource
 admin.site.register(Party1, ImportExportModelAdmin)
 admin.site.register(statepartyactivate1, ImportExportModelAdmin)
 admin.site.register(districtpartyactivate1, ImportExportModelAdmin)
