a26c770b787b40402ffaf5492ef71dfaa32b5898
mohan <adhikar940@gmail.com>
2022-04-01 09:29:01 +0530

    ac

--- assembly/admin.py
+++ assembly/admin.py
@@ -4,11 +4,11 @@ from . models import *
 from import_export import resources
 
 class Assembly_Constituency1Resource(resources.ModelResource):
-    search_fields = ['Assembly_Constituency_Name',]
     class Meta:
         model = Assembly_Constituency1
 class Assembly_Constituency1Admin(ImportExportModelAdmin):
     #list_display=('MLA_name',)
+    search_fields = ['Assembly_Constituency_Name',]
     list_filter=('State',)
     resource_class = Assembly_Constituency1Resource
 admin.site.register(Assembly_Constituency1, Assembly_Constituency1Admin)
