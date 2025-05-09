a310d7792efb88e5b89f40e7aefdec393d839dd5
mohan <mohangoud@sivisoft.com>
2025-05-09 19:56:06 +0530

    party changes

--- party/models.py
+++ party/models.py
@@ -10,15 +10,34 @@ class Party(models.Model):
         ('Regional', 'regional'),
         ('National', 'national'),
     )
+    PARTY_COLOR_CHOICES = [
+        ('#FF9933', 'BJP - Orange'),
+        ('#00A2E8', 'INC - Blue'),
+        ('#000000', 'CPI(M) - Black'),
+        ('#FF0000', 'CPI - Red'),
+        ('#008000', 'BSP - Green'),
+        ('#FFD700', 'TMC - Yellow'),
+        ('#FF4500', 'Shiv Sena - Saffron'),
+        ('#A52A2A', 'DMK - Brown'),
+        ('#000080', 'AIADMK - Navy Blue'),
+        ('#800080', 'AAP - Purple'),
+        ('#FFFFFF', 'None/Independent - White'),
+    ]
+    party_color = models.CharField(
+        max_length=7,
+        choices=PARTY_COLOR_CHOICES,
+        default='#FFFFFF',
+        help_text="Select party color"
+    )
     partystatus = models.CharField(max_length=30, choices=choice)
     partyname = models.CharField(max_length=100)
     abbreviation = models.CharField(null=True, max_length=20, unique=True)
     President = models.CharField(max_length=100, null=True)
     founder = models.CharField(max_length=100, null=True)
     chairperson = models.CharField(max_length=100, null=True)
-    founded_date = models.DateField()
+    foundeddate = models.DateField()
     headquarters = models.CharField(max_length=1000, null=True)
-    party_symbol = models.ImageField(upload_to='Party/party_symbol/%Y-%m-%d/%H-%M-%S', null=True)
+    partysymbol = models.ImageField(upload_to='Party/party_symbol/%Y-%m-%d/%H-%M-%S', null=True)
     founderPhoto = models.ImageField(upload_to='Party/founderPhoto/%Y-%m-%d/%H-%M-%S', null=True)
     chairpersonPhoto = models.ImageField(upload_to='Party/chairpersonPhoto/%Y-%m-%d/%H-%M-%S', null=True)
     PresidentPhoto = models.ImageField(upload_to='Party/PresidentPhoto/%Y-%m-%d/%H-%M-%S', null=True)
